0cec973b147cbcb7509630749f102af25661e07e
rcastley <rcastley@rcastley.home>
2014-10-02 17:38:24 +0100

    Missing timestamp

--- application/models/SummaryMapper.php
+++ application/models/SummaryMapper.php
@@ -149,7 +149,7 @@ class Application_Model_SummaryMapper
     public function fetchAllCount ()
     {
         $query = $this->getDbTable()
-        ->select('id')
+        ->select('id, timestamp')
         ->from(array(
                 's' => 'summary'
         ), array('COUNT(*) AS stotal'))

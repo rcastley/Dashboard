e45587ef0931a50655cf28f6efa4ab31cc804d1d
rcastley <robert.castley@gmail.com>
2015-06-16 09:25:21 +0100

    Code Tidy

--- library/Catchpoint/Types.php
+++ library/Catchpoint/Types.php
@@ -1,23 +1,23 @@
 <?php
-
 class Catchpoint_Types
 {
 
-    public function Types ($typeId)
-    {
-        $types = array(
-                0 => 'Web',
-                1 => 'Transaction',
-                2 => 'HTML Code',
-                3 => 'FTP',
-                4 => 'TCP',
-                5 => 'DNS',
-                6 => 'Ping',
-                7 => 'SMTP',
-                8 => 'UDP',
-                9 => 'API',
-                10 => 'Streaming'
-        );
-        return $types[$typeId];
-    }
+	public function Types ($typeId)
+	{
+		$types = array(
+			0 => 'Web',
+			1 => 'Transaction',
+			2 => 'HTML Code',
+			3 => 'FTP',
+			4 => 'TCP',
+			5 => 'DNS',
+			6 => 'Ping',
+			7 => 'SMTP',
+			8 => 'UDP',
+			9 => 'API',
+			10 => 'Streaming'
+		);
+
+		return $types[$typeId];
+	}
 }

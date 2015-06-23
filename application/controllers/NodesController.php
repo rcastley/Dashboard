664dae6955438c45688c7e7e755857b308c9de7a
rcastley <robert.castley@gmail.com>
2015-06-23 16:20:52 +0100

    Split out function into individual controllers

--- application/controllers/NodesController.php
+++ application/controllers/NodesController.php
@@ -3,27 +3,29 @@
 class NodesController extends Zend_Controller_Action
 {
 
-    protected $_summary;
-
-    public function init ()
-    {
-        $this->_summary = new Application_Model_SummaryMapper();
-    }
-
     public function indexAction ()
     {
+        $nodes = new Application_Model_NodesMapper();
+        $this->view->nodes = $nodes->fetchAll();
+    }
+
+    public function summaryAction ()
+    {
+        $summary = new Application_Model_SummaryMapper();
+
         $nodeId = $this->_getParam('nodeid');
-        $nodePerf = $this->_summary->nodePerf($nodeId);
+        $nodePerf = $summary->nodePerf($nodeId);
         
         $nodeArray = array();
-        
         $dataArray = array();
         
         foreach ($nodePerf as $p) { 
-                    $dataArray = null;        
+            $dataArray = null;        
+            
             $dataArray[] = array(
-                    $p->nodename, number_format($p->total, 0, '.', '')
+                $p->nodename, number_format($p->total, 0, '.', '')
             );
+
             $nodeArray[] = array('name' => $p->testname, 'data' => $dataArray);
         }
 

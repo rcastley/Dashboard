6a0903a345f48cdc466c298c02d112e659d78587
rcastley <robert.castley@gmail.com>
2016-01-07 08:43:01 +0000

    Fixes

--- application/controllers/TestsController.php
+++ application/controllers/TestsController.php
@@ -12,9 +12,9 @@ class TestsController extends Zend_Controller_Action
 	public function init ()
 	{
 		$this->_tests = new Application_Model_TestsMapper();
-		
+
 		$this->_nodes = new Application_Model_NodesMapper();
-		
+
 		$this->_summary = new Application_Model_SummaryMapper();
 	}
 
@@ -22,20 +22,20 @@ class TestsController extends Zend_Controller_Action
 	{
 		$this->view->monitor = new Catchpoint_Monitors();
 		$this->view->type = new Catchpoint_Types();
-		
+
 		$tests = $this->_tests->fetchAll();
-		
+
 		$this->view->tests = $tests;
 
 		foreach ($tests as $test) {
-			
+
 			$data = $this->_summary->dailyPerf($test->id);
 
 			if(count($data) == 0) {
 				$this->_tests->delete($test->id);
 			} else {
-				$dataArray = null; 
-			
+				$dataArray = null;
+
 				foreach ($data as $d) {
 					$dataArray[] =  number_format($d->total, 0, '.', '');
 				}
@@ -49,41 +49,41 @@ class TestsController extends Zend_Controller_Action
 
 	public function failedAction ()
 	{
-		$getError = new Catchpoint_Errors();
-		
-		$this->view->error = $getError;
-		$this->view->tests = $this->_summary->fetchFailed('-24 hours');	
+		//$getError = new Catchpoint_Errors();
+
+		//$this->view->error = $getError;
+		$this->view->tests = $this->_summary->fetchFailed('-24 hours');
 	}
 
 	public function detailAction ()
 	{
 		$testId = $this->_getParam('id');
-		
+
 		$testName = $this->_tests->fetchRow($testId);
-		
+
 		$data = $this->_summary->dailyPerf($testId, '-7 days');
 
 		$this->view->lastWeek = $this->_summary->comparePerf($testId, '-2 days', '-1 day');
 
 		$this->view->thisWeek = $this->_summary->comparePerf($testId, '-1 day', '-0 hour');
-		
+
 		$testArray = array();
 
 		$dataArray = array();
-			
+
 			foreach ($data as $d) {
-				$dataArray[] = array( 
+				$dataArray[] = array(
 					$d->interval, number_format($d->total, 0, '.', '')
 				);
 			}
 
 		$testArray[] = array('name' => $testName->name, 'data' => $dataArray);
-		
-		
+
+
 		$this->view->testName = $testName->name;
 
 		$this->view->chartData = json_encode($testArray, JSON_NUMERIC_CHECK);
-		
+
 		$city = $this->_summary->perfByCity($testId, '-7 day');
 
 		foreach ($city as $c) {
@@ -97,7 +97,7 @@ class TestsController extends Zend_Controller_Action
 			$cityArray[] = array('name' => $c->city . ' - ' . $c->carrier, 'data' => $mapArray);
 		}
 
-		$this->view->mapData = json_encode($cityArray, JSON_NUMERIC_CHECK);        
+		$this->view->mapData = json_encode($cityArray, JSON_NUMERIC_CHECK);
 	}
 
 }
--- library/Catchpoint/Rag.php
+++ library/Catchpoint/Rag.php
@@ -8,30 +8,30 @@ class Catchpoint_Rag
         if ($perf > 0 && $perf <= 2000) {
             $button = '<button type="button" class="btn btn-xs btn-success">' . number_format($perf) . ' ms</button>';
         }
-        
-        if ($perf >2000 && $perf <=5000 ) {
+
+        if ($perf >2000 && $perf <=8000 ) {
             $button = '<button type="button" class="btn btn-xs btn-warning">' . number_format($perf) . ' ms</button>';
         }
-        
-        if ($perf >4000) {
+
+        if ($perf >10000) {
             $button = '<button type="button" class="btn btn-xs btn-danger">' . number_format($perf) . ' ms</button>';
         }
         return $button;
     }
-    
+
     public function Avail ($avail){
         if ($avail == 100) {
             $button = '<button type="button" class="btn btn-xs btn-success">' . $avail . '%</button>';
         }
-        
+
         if ($avail <100 && $avail >= 99 ) {
             $button = '<button type="button" class="btn btn-xs btn-warning">' . $avail . ' %</button>';
         }
-        
+
         if ($avail < 99) {
             $button = '<button type="button" class="btn btn-xs btn-danger">' . $avail . ' %</button>';
         }
         return $button;
-        
+
     }
 }
